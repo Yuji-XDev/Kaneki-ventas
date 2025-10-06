499796b57308acb11ece88ec310be396a7a69985
the-27 <blackoficial2025@gmail.com>
2025-10-06 11:10:14 -0500

    Update

--- plugins/hola.js
+++ plugins/hola.js
@@ -1,7 +1,7 @@
-export default async function (sock, m, text) {
+/*export default async function (sock, m, text) {
   if (text.toLowerCase().includes('hola')) {
     await sock.sendMessage(m.key.remoteJid, {
       text: '👋 ¡Hola! Bienvenido a *Kaneki Ventas* 🛒\nEscribe *menu* para ver nuestros productos xd.'
     })
   }
-}
+}*/
